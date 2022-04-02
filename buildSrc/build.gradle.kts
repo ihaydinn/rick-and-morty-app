090f48b94d9323c9e2ab1c3094e53de7c6f7478e
ihaydinn <ihaydin7@gmail.com>
2022-04-02 18:17:34 +0300

    Add ktlint

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,3 +8,14 @@ repositories {
     maven("https://plugins.gradle.org/m2/")
     maven("https://jitpack.io")
 }
+
+object PluginVersion {
+    const val KTLINT = "10.0.0"
+    const val KOTLIN = "1.6.10"
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:7.0.4")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginVersion.KOTLIN}")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:${PluginVersion.KTLINT}")
+}
